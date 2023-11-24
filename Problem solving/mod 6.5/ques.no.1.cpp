0604844035b8fbcc6b2ee9516c2df76076f9a308
Khaled Karim <kskarim177@gmail.com>
2023-11-24 18:55:05 +0600

    cheers on solving the first prob of graph

---
+++ Problem solving/mod 6.5/ques.no.1.cpp
@@ -0,0 +1,44 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// const int N = 1e5 + 5;
+// vector<int> adj[N];
+// bool visited[N];
+
+// void dfs(int u)
+// {
+//     visited[u] = true;
+//     for (int v : adj[u])
+//     {
+//         if (visited[v] == true)
+//         {
+//             continue;
+//         }
+//         dfs(v);
+//     }
+// }
+
+// int main()
+// {
+//     int n, m;
+//     cin >> n >> m;
+//     for (int i = 0; i < m; i++)
+//     {
+//         int u, v;
+//         cin >> u >> v;
+//         adj[u].push_back(v);
+//         adj[v].push_back(u);
+//     }
+//     dfs(1);
+
+//     for (int i = 1; i <= n; i++)
+//     {
+
+//         for (int j = 1; j <= n; j++)
+//         {
+//             cout << adjmat[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
---
+++ Problem solving/mod 6.5/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         cout << adjmat[i][j] << " ";
+    //         if (adjmat[i][j] == 1)
+    //         {
+    //             adj[i].push_back(j);
+    //         }
+    //     }
+    //     cout << endl;
+    // }
