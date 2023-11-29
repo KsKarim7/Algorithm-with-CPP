a38df081765a8afa0fc8c118c1c329f064d76919
Khaled Karim <kskarim177@gmail.com>
2023-11-30 03:24:01 +0600

    corner cases are not corning

---
+++ Problem solving/Assignment 2/neighbourhood-1.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e3 + 5;
+vector<int> adj[N];
+bool visited[N];
+
+void dfs(int u, int r, int &count)
+{
+
+    visited[u] = true;
+    // cout << "Visiting node: " << u << endl;
+    for (int v : adj[u])
+    {
+        if (r == v)
+        {
+            count++;
+        }
+        if (visited[v] == true)
+        {
+            continue;
+        }
+        dfs(v, r, count);
+    }
+}
+
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    for (int i = 0; i < n; i++)
+    {
+
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    int k;
+    cin >> k;
+    int count = 0;
+    dfs(1, k, count);
+    cout << count;
+    return 0;
+}
