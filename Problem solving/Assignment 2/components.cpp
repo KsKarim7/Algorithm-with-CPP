3086ed4e639e3137dcbd3b95faa8bf477a0be61d
Khaled Karim <kskarim177@gmail.com>
2023-12-12 18:31:21 +0600

    components

--- Problem solving/Assignment 2/components.cpp
+++ Problem solving/Assignment 2/components.cpp
@@ -4,85 +4,99 @@ using namespace std;
 int const N = 1e5 + 5;
 vector<int> adj[N];
 bool visited[N];
+vector<int> ans;
+int c = 0;
 
-void dfs(int u, int &num)
+void dfs(int u)
 {
-    num++;
     visited[u] = true;
+    c++;
     for (int v : adj[u])
     {
-
         if (visited[v])
         {
             continue;
         }
-        dfs(v, num);
+        dfs(v);
     }
 }
 
-void bfs(int s)
-{
-    queue<int> q;
-    q.push(s);
-    visited[s] = true;
+// void bfs(int s)
+// {
+//     queue<int> q;
+//     q.push(s);
+//     visited[s] = true;
 
-    while (!q.empty())
-    {
-        int u = q.front();
-        q.pop();
-        for (int v : adj[u])
-        {
-            if (visited[v])
-                continue;
-            q.push(v);
-            visited[v] = true;
-        }
-    }
-}
+//     while (!q.empty())
+//     {
+//         int u = q.front();
+//         q.pop();
+//         for (int v : adj[u])
+//         {
+//             if (visited[v])
+//                 continue;
+//             q.push(v);
+//             visited[v] = true;
+//         }
+//     }
+// }
 
 int main()
 {
     int n, m;
     cin >> n >> m;
-    int arr[n + 1];
-    bool visit[N];
-    int j = 1;
+    // int arr[n + 1];
+    // bool visit[N];
+    // int j = 1;
     for (int i = 0; i < m; i++)
     {
         int u, v;
         cin >> u >> v;
-        if (!visit[u])
-        {
-            visit[u] = true;
-            arr[j] = u;
-            j++;
-        }
-        if (!visit[v])
-        {
-            visit[v] = true;
-            arr[j] = v;
-            j++;
-        }
+        // if (!visit[u])
+        // {
+        //     visit[u] = true;
+        //     arr[j] = u;
+        //     j++;
+        // }
+        // if (!visit[v])
+        // {
+        //     visit[v] = true;
+        //     arr[j] = v;
+        //     j++;
+        // }
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    int conComp = 0;
-    vector<int> vec;
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < N; i++)
     {
-        int num = 0;
-        if (visited[arr[i]])
+        if (visited[i])
         {
-            // num++;
             continue;
         }
-        dfs(arr[i], num);
-        vec.push_back(num);
-        conComp++;
-        // num = 0;
+        c = 0;
+        dfs(i);
+        if (c > 1)
+        {
+            ans.push_back(c);
+        }
     }
-    sort(vec.begin(), vec.end());
-    for (auto x : vec)
+    // int conComp = 0;
+    // vector<int> vec;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     int num = 0;
+    //     if (visited[arr[i]])
+    //     {
+    //         // num++;
+    //         continue;
+    //     }
+    //     dfs(arr[i], num);
+    //     vec.push_back(num);
+    //     conComp++;
+    //     // num = 0;
+    // }
+    sort(ans.begin(), ans.end());
+    for (auto x : ans)
         cout << x << " ";
     return 0;
 }
