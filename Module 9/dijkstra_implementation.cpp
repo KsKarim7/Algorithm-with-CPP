df4ae8ad75f280dc0147650c8d994cc494dfc9dd
Khaled Karim <kskarim177@gmail.com>
2023-12-13 13:32:15 +0600

    dijkstra algorithm

---
+++ Module 9/dijkstra_implementation.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> pr;
+
+const int N = 1e3 + 5;
+const int INF = 1e9 + 10;
+
+vector<pr> adj[N];
+vector<int> dis(N, INF);
+vector<bool> visited(N);
+
+void dijkstra(int source)
+{
+    priority_queue<pr, vector<pr>, greater<pr>> pq;
+    dis[source] = 0;
+    pq.push({dis[source], source});
+
+    while (!pq.empty())
+    {
+        int u = pq.top().second;
+        pq.pop();
+
+        visited[u] = true;
+
+        for (pr vpair : adj[u])
+        {
+            int v = vpair.first;
+            int w = vpair.second;
+
+            if (visited[v])
+            {
+                continue;
+            }
+
+            if (dis[v] > dis[u] + w)
+            {
+                dis[v] = dis[u] + w;
+                pq.push({dis[v], v});
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+    dijkstra(1);
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Distance of node " << i << ": ";
+        cout << dis[i] << endl;
+    }
+    return 0;
+}
---
+++ Module 9/shortest_path_bfs.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+vector<int> adj[N];
+bool visited[N];
+int level[N];
+int parent[N];
+
+// here the time complexity of bfs is O(n+m)  - edge
+void bfs(int s)
+{
+    queue<int> q;
+    q.push(s);
+    visited[s] = true;
+    level[s] = 0;
+    parent[s] = -1;
+
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        for (int v : adj[u])
+        {
+            if (visited[v] == false)
+            {
+                q.push(v);
+                visited[v] = true;
+                level[v] = level[u] + 1;
+                parent[v] = u;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int m, n, s, d;
+    cin >> m >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    cin >> s >> d;
+    bfs(s);
+    cout << "Distance: " << level[d] << endl;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     cout << "Parent of " << i << ": " << parent[i];
+    //     cout << endl;
+    // }
+
+    //  worst case time complexity for finding path is O(n)
+    vector<int> path;
+    int current = d;
+    while (current != -1)
+    {
+        path.push_back(current);
+        current = parent[current];
+    }
+    reverse(path.begin(), path.end());
+    cout << "Path : ";
+    for (int node : path)
+    {
+        cout << node << " ";
+    }
+
+    return 0;
+}
