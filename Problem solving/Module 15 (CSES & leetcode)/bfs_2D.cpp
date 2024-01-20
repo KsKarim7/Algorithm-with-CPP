ba47bfdc22fc112ed072efa42d0731ebaaeaf6e2
Khaled Karim <kskarim177@gmail.com>
2024-01-20 23:59:57 +0600

    test commit

---
+++ Problem solving/Module 15 (CSES & leetcode)/bfs_2D.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e3 + 5;
+bool vis[N][N];
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    char arr[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    int si, sj;
+    cin >> si >> sj;
+    return 0;
+}
