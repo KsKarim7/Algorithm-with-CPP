f5366959c8e5308663811f636dca07cf8436c457
Khaled Karim <kskarim177@gmail.com>
2024-02-05 17:54:58 +0600

    mid term submission

--- Problem solving/Mid Term/knight_moves.cpp
+++ Problem solving/Mid Term/knight_moves.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define pi pair<int, int>
+#define pr pair<int, int>
 
-int t, n, m, k, l, u, r, step;
+int t, n, m, ki, kj, qi, qj;
 
 const int N = 1e2 + 5;
 bool vis[N][N];
 int dis[N][N];
-// vector<pi> path = {{2, 1}, {1, 2}, {-1, 2}, {-1, 2}, {1, -2}, {2, -1}, {-2, -1}};
-vector<pi> path = {{1, -2}, {2, -1}, {2, 1}, {1, 2}, {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}};
+
+vector<pr> path = {{1, -2}, {2, -1}, {2, 1}, {1, 2}, {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}};
 
 bool isValid(int cI, int cJ)
 {
@@ -18,34 +18,28 @@ bool isValid(int cI, int cJ)
         return false;
 }
 
-void dfs(int si, int sj)
+void bfs(int si, int sj)
 {
-    queue<pi> q;
+    queue<pr> q;
     q.push({si, sj});
     dis[si][sj] = 0;
     vis[si][sj] = true;
     while (!q.empty())
     {
-        pi parent = q.front();
+        pr parent = q.front();
+        q.pop();
         int pI = parent.first;
         int pJ = parent.second;
-        q.pop();
         for (int i = 0; i < path.size(); i++)
         {
-            pi p = path[i];
+            pr p = path[i];
             int cI = pI + p.first;
             int cJ = pJ + p.second;
             if (isValid(cI, cJ) && !vis[cI][cJ])
             {
-                step++;
-                if (cI == u && cJ == r)
-                {
-                    break;
-                }
                 vis[cI][cJ] = true;
                 q.push({cI, cJ});
                 dis[cI][cJ] = dis[pI][pJ] + 1;
-                // cout << "step" << endl;
             }
         }
     }
@@ -54,17 +48,39 @@ void dfs(int si, int sj)
 int main()
 {
     cin >> t;
-    cin >> n >> m;
-    int arr[n][m];
     for (int i = 0; i < t; i++)
     {
-        step = 0;
-        cin >> k >> l;
-        cin >> u >> r;
-        dfs(k, l);
-        cout << step << endl;
-        // cout << dis[k][l] << endl;
+        cin >> n >> m;
+        cin >> ki >> kj;
+        cin >> qi >> qj;
+        bfs(ki, kj);
+        // if (i == 0)
+        // {
+        //     for (int i = 0; i < n; i++)
+        //     {
+        //         for (int j = 0; j < m; j++)
+        //         {
+        //             cout << dis[i][j] << " ";
+        //         }
+        //         cout << endl;
+        //     }
+        // }
+        if (vis[qi][qj])
+        {
+            cout << dis[qi][qj] << endl;
+        }
+        else
+        {
+            cout << -1 << endl;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                dis[i][j] = 0;
+                vis[i][j] = false;
+            }
+        }
     }
-
     return 0;
 }
