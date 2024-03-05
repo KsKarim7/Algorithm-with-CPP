4123311e7e0a6ec99df012f8afc480796c447b23
Khaled Karim <kskarim177@gmail.com>
2024-03-05 12:47:01 +0600

    exma_marks submitted(problem)

--- Problem solving/Assignment 4/exam_marks.cpp
+++ Problem solving/Assignment 4/exam_marks.cpp
@@ -1,12 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int dp[1005][1005];
+
+bool if_get(int n, int s, int arr[])
+{
+    if (n == 0)
+    {
+        if (s == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    if (dp[n][s] != -1)
+    {
+        return dp[n][s];
+    }
+    if (arr[n - 1] <= s)
+    {
+        bool option1 = if_get(n - 1, s - arr[n - 1], arr);
+        bool option2 = if_get(n - 1, s, arr);
+        return dp[n][s] = option1 || option2;
+    }
+    else
+    {
+        return dp[n][s] = if_get(n - 1, s, arr);
+    }
+}
+
 int main()
 {
     int t, n, m;
     cin >> t;
     for (int i = 0; i < t; i++)
     {
-        bool flag = false;
         cin >> n >> m;
         int arr[n];
         for (int j = 0; j < n; j++)
@@ -16,23 +47,25 @@ int main()
         if (m == 1000)
         {
             cout << "YES" << endl;
-            flag = true;
         }
         else
         {
-            for (int j = 0; j < n; j++)
+            int s = 1000 - m;
+            for (int i = 0; i <= n; i++)
             {
-                m += arr[j];
-                if (m == 1000)
+                for (int j = 0; j <= s; j++)
                 {
-                    cout << "YES" << endl;
-                    flag = true;
+                    dp[i][j] = -1;
                 }
             }
-        }
-        if (flag == false)
-        {
-            cout << "NO" << endl;
+            if (if_get(n, s, arr))
+            {
+                cout << "YES" << endl;
+            }
+            else
+            {
+                cout << "NO" << endl;
+            }
         }
     }
     return 0;
