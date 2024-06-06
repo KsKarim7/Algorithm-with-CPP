3d5728456c014b1718389e14e1621fa8627d717a
Khaled Karim <kskarim177@gmail.com>
2024-06-06 12:59:24 +0600

    beginning of 221 lab

---
+++ Problem solving/CSE221/Lab one/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 55, b = 60, c = 70;
+
+    double avgResult;
+
+    avgResult = ((a + b + c) / 3);
+
+    cout << avgResult; // should display 61.666666
+
+    return 0;
+}
